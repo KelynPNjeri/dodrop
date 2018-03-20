c1fcef722dcb8baf7d61facf5ac32a7e0cc2a931
Lenny Kamande <lennymanyeki@gmail.com>
2018-03-20 19:58:56 +0300

    initial commit

---
+++ app/src/main/java/com/simcoder/uber/PayPalConfig.java
@@ -0,0 +1,9 @@
+package com.simcoder.uber;
+
+/**
+ * Created by Sony on 3/17/2018.
+ */
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "Aekemn84MHtuWnhxB69wtACiJEjHJIKho5KXwJpg3bV-YnbeRIju9r5P1BvxwB754fQRfDBgf5DRQCxc";
+}
